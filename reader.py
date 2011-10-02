0e836bebc26f0ebea3a78f2ce56bf7388f1b0651
Krzysztof Urban <krz.urb@gmail.com>
2011-10-03 01:29:08 +0200

    some more verbosity

--- reader.py
+++ reader.py
@@ -11,6 +11,7 @@ print "Loading file '{0}'...".format(filename)
 
 with open(filename) as f:
     nr_of_rows = int(f.readline())
+    print "Instance size: {0}".format(nr_of_rows)
     f.readline()
     for i in xrange(nr_of_rows):
         row = f.readline().split()
