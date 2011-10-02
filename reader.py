0a4c478c8e5848344c7e358ac5fbc2b261df00c0
Krzysztof Urban <krz.urb@gmail.com>
2011-10-03 01:24:12 +0200

    simple instance reader with example instance

---
+++ reader.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+filename = sys.argv[1]
+
+flow = []
+distance = []
+
+print "Loading file '{0}'...".format(filename)
+
+with open(filename) as f:
+    nr_of_rows = int(f.readline())
+    f.readline()
+    for i in xrange(nr_of_rows):
+        row = f.readline().split()
+        flow.append(row)
+    f.readline()
+    for i in xrange(nr_of_rows):
+        row = f.readline().split()
+        distance.append(row)
+
+print "File loaded!"
