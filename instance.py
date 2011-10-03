b61f5fe089cab8eaa4ac69c23902e828cfa8d29c
Krzysztof Urban <krz.urb@gmail.com>
2011-10-03 17:35:19 +0200

    class for QAP instances

---
+++ instance.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Instance(object):
+
+    def __init__(self, filename):
+        self.flow = []
+        self.distance = []
+        print "Loading file '{0}'...".format(filename)
+        with open(filename) as f:
+            self.nr_of_rows = int(f.readline())
+            print "Instance size: {0}".format(nr_of_rows)
+            f.readline()
+            for i in xrange(nr_of_rows):
+                row = f.readline().split()
+                self.flow.append([float(x) for x in row])
+            f.readline()
+            for i in xrange(nr_of_rows):
+                row = f.readline().split()
+                self.distance.append([float(x) for x in row])
+        print "File loaded!"
+
+    def __len__(self):
+        return self.nr_of_rows
