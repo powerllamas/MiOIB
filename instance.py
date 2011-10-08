5a511ad2fbb56611cc977c50797c0ff2db77229a
Krzysztof Urban <krz.urb@gmail.com>
2011-10-08 20:35:56 +0200

    Fixed instance loader

--- instance.py
+++ instance.py
@@ -8,13 +8,13 @@ class Instance(object):
         print "Loading file '{0}'...".format(filename)
         with open(filename) as f:
             self.nr_of_rows = int(f.readline())
-            print "Instance size: {0}".format(nr_of_rows)
+            print "Instance size: {0}".format(self.nr_of_rows)
             f.readline()
-            for i in xrange(nr_of_rows):
+            for i in xrange(self.nr_of_rows):
                 row = f.readline().split()
                 self.flow.append([float(x) for x in row])
             f.readline()
-            for i in xrange(nr_of_rows):
+            for i in xrange(self.nr_of_rows):
                 row = f.readline().split()
                 self.distance.append([float(x) for x in row])
         print "File loaded!"
