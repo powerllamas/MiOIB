607aa2a4183b3f3386c9a1ff04aa851da2b919bd
Tomek <tomek.zietkiewicz@gmail.com>
2011-10-10 12:57:32 +0200

    Neighbourhood generation

--- solution.py
+++ solution.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
+from itertools import combinations
 
 class Solution(object):
 
     def __init__(self, sequence):
         self.sequence = sequence
-
+        
+    def neighbours(self):
+        for x, y in combinations(self.sequence, 2):
+            old = list(self.sequence)
+            old[x], old[y] = old[y], old[x]
+            neighbour = Solution(tuple(old))
+            yield neighbour
+            
+    def __str__(self):
+        return str(self.sequence)
+        
+    def __repr__(self):
+        return str(self)
