cebc4c1efa162bfd0fbe1026bf9276d0764d1e38
Tomek <tomek.zietkiewicz@gmail.com>
2011-10-17 15:24:39 +0200

    Added heuristic, tests for heuristic and changed instance constructor

--- instance.py
+++ instance.py
@@ -2,22 +2,42 @@
 
 class Instance(object):
 
-    def __init__(self, filename):
-        self.flow = []
-        self.distance = []
-        print "Loading file '{0}'...".format(filename)
-        with open(filename) as f:
-            self.nr_of_rows = int(f.readline())
-            print "Instance size: {0}".format(self.nr_of_rows)
-            f.readline()
-            for i in xrange(self.nr_of_rows):
-                row = f.readline().split()
-                self.flow.append([float(x) for x in row])
-            f.readline()
-            for i in xrange(self.nr_of_rows):
-                row = f.readline().split()
-                self.distance.append([float(x) for x in row])
-        print "File loaded!"
+    # def __init__(self, filename):
+        # self.flow = []
+        # self.distance = []
+        # print "Loading file '{0}'...".format(filename)
+        # with open(filename) as f:
+            # self.nr_of_rows = int(f.readline())
+            # print "Instance size: {0}".format(self.nr_of_rows)
+            # f.readline()
+            # for i in xrange(self.nr_of_rows):
+                # row = f.readline().split()
+                # self.flow.append([float(x) for x in row])
+            # f.readline()
+            # for i in xrange(self.nr_of_rows):
+                # row = f.readline().split()
+                # self.distance.append([float(x) for x in row])
+        # print "File loaded!"
 
+    def __init__(self, filename = None, distance = [], flow = []):
+        self.flow = flow
+        self.distance = distance
+        if distance == [] or flow == []:
+            print "Loading file '{0}'...".format(filename)
+            with open(filename) as f:
+                self.nr_of_rows = int(f.readline())
+                print "Instance size: {0}".format(self.nr_of_rows)
+                f.readline()
+                for i in xrange(self.nr_of_rows):
+                    row = f.readline().split()
+                    self.flow.append([float(x) for x in row])
+                f.readline()
+                for i in xrange(self.nr_of_rows):
+                    row = f.readline().split()
+                    self.distance.append([float(x) for x in row])
+            print "File loaded!"
+        
     def __len__(self):
-        return self.nr_of_rows
+        return self.nr_of_rows    
+
+        
