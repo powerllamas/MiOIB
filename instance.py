bcd5c2650333c8cbe5f6409684f0aa92cbd4a7d2
Krzysztof Urban <krz.urb@gmail.com>
2011-10-18 21:56:17 +0200

    same here

--- instance.py
+++ instance.py
@@ -2,32 +2,14 @@
 
 class Instance(object):
 
-    # def __init__(self, filename):
-        # self.flow = []
-        # self.distance = []
-        # print "Loading file '{0}'...".format(filename)
-        # with open(filename) as f:
-            # self.nr_of_rows = int(f.readline())
-            # print "Instance size: {0}".format(self.nr_of_rows)
-            # f.readline()
-            # for i in xrange(self.nr_of_rows):
-                # row = f.readline().split()
-                # self.flow.append([float(x) for x in row])
-            # f.readline()
-            # for i in xrange(self.nr_of_rows):
-                # row = f.readline().split()
-                # self.distance.append([float(x) for x in row])
-        # print "File loaded!"
-
     def __init__(self, filename = None, distance = [], flow = []):
         self.flow = []
         self.distance = []
         if filename == None:
             self.flow = flow
             self.distance = distance
-            self.nr_of_rows = len(flow)       
+            self.nr_of_rows = len(flow)
         else:
-            #print "Loading file '{0}'...".format(filename)
             with open(filename) as f:
                 self.nr_of_rows = int(f.readline())
                 print "Instance size: {0}".format(self.nr_of_rows)
@@ -39,9 +21,7 @@ class Instance(object):
                 for i in xrange(self.nr_of_rows):
                     row = f.readline().split()
                     self.distance.append([float(x) for x in row])
-            #print "File loaded!"
-        
-    def __len__(self):
-        return self.nr_of_rows    
 
-        
+    def __len__(self):
+        return self.nr_of_rows
+
