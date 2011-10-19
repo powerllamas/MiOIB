6ee49a6de2ae70aab912b0357854a4e16e1eda60
Krzysztof Urban <krz.urb@gmail.com>
2011-10-19 13:08:46 +0200

    evaluator is now cached (could be turned off)

--- evaluator.py
+++ evaluator.py
@@ -3,10 +3,14 @@
 
 class Evaluator(object):
 
-    def __init__(self, instance):
+    def __init__(self, instance, cached=True):
         self.instance = instance
+        self.cached = cached
+        self.cache = {}
 
     def evaluate(self, solution):
+        if solution.sequence in self.cache:
+            return self.cache[solution.sequence]
         score = 0
         flow = self.instance.flow
         distance = self.instance.distance
@@ -14,4 +18,6 @@ class Evaluator(object):
         for y in xrange(len(self.instance)):
             for x in xrange(len(self.instance)):
                 score += flow[y][x] * distance[sequence[y]][sequence[x]]
+        if self.cached:
+            self.cache[solution.sequence] = score
         return score
