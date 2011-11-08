a2f44499c052e409f3dfc62d1302ac6c220364bf
Tomek <tomek.zietkiewicz@gmail.com>
2011-11-08 02:42:19 +0100

    Added solutions similarity comparision

---
+++ similarity.py
@@ -0,0 +1,37 @@
+﻿import solution
+import instance
+import evaluator
+
+def binary_similarity(sequence1, sequence2):
+    similarity = 0.0
+    for e1, e2 in zip(sequence1, sequence2):
+        if e1 == e2:
+            similarity += 1.0
+    return similarity / float(len(min(sequence1, sequence2)))
+
+def ratio_similarity(vector1, vector2):
+    similarity = 0.0
+    for v1, v2 in zip(vector1, vector2):        
+        ratio = float(min(v1, v2)) / float(max(v1,v2))
+        similarity += ratio
+    similarity = similarity / float(len(vector1))
+    return similarity
+    
+def binary_solution_similarity(solution1, solution2):
+    return binary_similarity(solution1.sequence, solution2.sequence)
+   
+def partial_solution_similarity(solution1, solution2, instance):
+    similarity = 0.0
+    for e1, e2 in zip(solution1.sequence, solution2.sequence):
+        if e1 == e2:
+            similarity += 1.0
+        else:
+            distances_to1 = instance.distance[e1]
+            distances_to2 = instance.distance[e2]
+            similarity += ratio_similarity(distances_to1, distances_to2) / float(2)
+            ratio = ratio_similarity(distances_to1, distances_to2) / float(2)
+            distances_from1 = [vector[e1] for vector in instance.distance]
+            distances_from2 = [vector[e2] for vector in instance.distance]
+            similarity += ratio_similarity(distances_from1, distances_from2) / float(2)
+    return float(similarity) / float(len(solution1.sequence))
+            
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from instance import Instance
 from heuristic import Heuristic
 from solution import Solution
 from local_search import LocalSearch
+import similarity
 
 
 class Test_heuristic(unittest.TestCase):
@@ -81,6 +82,61 @@ class TestLocalSearch(unittest.TestCase):
         expected = (2, 1, 0)
         actual = self.ls_greedy.solve(self.i, self.startpoint).sequence
         self.assertEqual(expected, actual)
+        
+class SimilarityTest(unittest.TestCase):
+    def setUp(self):
+        self.s1 = Solution((0,1,2))
+        self.s2 = Solution((0,2,1))
+        self.s3 = Solution((0,1,2))
+        self.flow = [
+                [0, 1, 2],
+                [4, 0, 5],
+                [7, 2, 0]]
+        self.distance = [
+                [0, 5, 5],
+                [4, 2, 2],
+                [4, 2, 2]]
+        self.i = Instance(None, self.distance, self.flow)
+        
+        self.flow2 = [
+                [0, 1, 2],
+                [4, 0, 5],
+                [7, 2, 0]]
+        self.distance2 = [
+                [0, 5, 2],
+                [1, 0, 1],
+                [6, 2, 0]]
+        self.i2 = Instance(None, self.distance2, self.flow2)
+        
+    def test_binary_similarity(self):
+        expected = 0.3333333333333333
+        actual = similarity.binary_solution_similarity(self.s1, self.s2)
+        self.assertAlmostEqual(actual, expected)
+        
+    def test_partial_solution_similarity_identical(self):
+        expected = 1.0
+        actual = similarity.binary_solution_similarity(self.s1, self.s3)
+        self.assertEqual(actual, expected)
+        
+    def test_ratio_similarity(self):
+        expected = (1.0 + 2.0/3.0 + 0.5) / 3.0
+        actual = similarity.ratio_similarity([3, 4, 2], [3, 6, 1])
+        self.assertEqual(actual, expected)
+        
+    def test_partial_solution_similarity_identical(self):
+        expected = 1.0
+        actual = similarity.partial_solution_similarity(self.s1, self.s2, self.i)
+        self.assertEqual(actual, expected)
+        
+    def test_partial_solution_similarity_identical2(self):
+        expected = 1.0
+        actual = similarity.partial_solution_similarity(self.s1, self.s3, self.i2)
+        self.assertEqual(actual, expected)
+        
+    def test_partial_solution_similarity(self):
+        expected = (1.0 + 1.0/18.0 + 4.0/30.0 ) / 3.0
+        actual = similarity.partial_solution_similarity(self.s1, self.s2, self.i2)
+        self.assertAlmostEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
